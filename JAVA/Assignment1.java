99869ef406cd21dcde4d0e15c4ce48601a843b36
Dheeraj Lalwani <52416311+dheerajdlalwani@users.noreply.github.com>
2020-09-08 19:50:52 +0530

    Update Assignment1.java

--- JAVA/Assignment1.java
+++ JAVA/Assignment1.java
@@ -5,5 +5,6 @@ package com.company;
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("This is Pranav Dani, a SE student.");
+     System.out.println("This is Dheeraj, a SE student.");
     }
 }
